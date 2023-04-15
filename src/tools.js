8bf1e24b38b0d35ef4907e025633925445a40b50
Vojtech Basta <vojtech@nabastu.cz>
2023-04-15 21:12:38 +0200

    Update tools.js

--- src/tools.js
+++ src/tools.js
@@ -232,7 +232,22 @@ export async function detailPageExtractProperties({ page, dataset, url }) {
         return liItems.length;
     });
     console.log("liLength",liLength);
-
+    extras.push(await page.evaluate(async () => {
+        var ul = document.querySelector(".ob-c-horizontal-scrolling-menu").parentElement.parentElement.parentElement.querySelectorAll("ul")[1];
+        console.log("ul",ul);
+        var liItems = ul.querySelectorAll("li").map((li) => {
+            return {
+                "key":li.querySelector("label").innerText,
+                "value":{
+                    "name":li.querySelector("a").innerText,
+                    "url":li.querySelector("a").href,
+                    "distance":li.querySelectorAll("span")[2].innerText
+                }
+            }
+        });
+        return {"menu":liName,"values":liItems};
+    }));
+    console.log("extras",extras);
     // for(var i = 0; i<liLength; i++){
     //     var liName = await page.evaluate(async () => {
     //         var liItems = document.querySelectorAll("preact[component='public-equipment'] .ob-c-horizontal-scrolling-menu__content ul li");
